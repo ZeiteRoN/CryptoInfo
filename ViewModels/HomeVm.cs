501d326af6f7a714a78e9661c115d04fbc5e41cd
ZeiteRoN <zeiter1007@gmail.com>
2024-11-01 19:23:42 +0200

    added HomeVm + Design

--- ViewModels/HomeVm.cs
+++ ViewModels/HomeVm.cs
@@ -1,7 +1,37 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CryptoInfo.Models;
+using CryptoInfo.Services;
+
 namespace CryptoInfo.ViewModels
 {
-    public class HomeVm
+    public class HomeVm: INotifyPropertyChanged
     {
-        
+        private readonly CryptoService _service;
+        public ObservableCollection<CryptoCurrency> currencies;
+
+        public HomeVm()
+        {
+            _service = new CryptoService();
+            currencies = new ObservableCollection<CryptoCurrency>();
+            LoadTopCryptos();
+        }
+
+        public async void LoadTopCryptos()
+        {
+            var cryptos = await _service.GetCryptoCurrencies();
+            currencies.Clear();
+            foreach (var crypto in cryptos)
+            {
+                currencies.Add(crypto);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
